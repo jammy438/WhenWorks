fa962b2b7179f8b9c76724c1c78a1deda9b49ac0
Sarah Love <144811442+sarahlove-sbg@users.noreply.github.com>
2025-08-05 15:38:49 +0100

    Impliment base in events.py and user.py

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -1,8 +1,7 @@
 from sqlalchemy import Column, Integer, String, DateTime, func
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
-
-Base = declarative_base()
+from app.models.base import Base # Import Base from the database module
 
 class User(Base):
     __tablename__ = 'users'
