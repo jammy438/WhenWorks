50c02c45f7abd672895266703f67ebce79e05281
Sarah Love <144811442+sarahlove-sbg@users.noreply.github.com>
2025-08-09 11:01:23 +0100

    test for the events api route

---
+++ backend/app/tests/routes/test_events.py
@@ -0,0 +1,11 @@
+#test that all events are gotten for current user
+#test that no events found returns 404 for current user
+#test that an event can be created for the current user
+#test that an event cannot be created with invalid data
+#test that an event can be updated for the current user
+#test that an event cannot be updated with invalid data
+#test that an event cannot be updated if it does not exist or belong to current user
+#test that an event can be deleted for the current user
+#test that an event cannot be deleted if it does not exist or belong to current user
+
+from 
